196421cd3ed6b35149e3a6a74a14dc17d4db9a7f
Yelyzaveta Musianovych <eliza.musichka@gmail.com>
2025-07-12 22:58:07 +0200

    fix bug

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     remotePatterns:[
-      { hostname: 'ac.goit.global' }
+      { protocol: 'https',hostname: 'ac.goit.global' }
     ]
   }
 };
